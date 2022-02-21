c86fdc6718befed6b766c065df94b2da5c0f7926
RERAN4K <newreran@gmail.com>
2022-02-21 12:16:44 +0300

    Third commit

--- codeworktime.py
+++ codeworktime.py
@@ -1,7 +1,7 @@
 def language(lan):
     global language
     language = lan
-def workitmebeg():
+def worktimebeg():
     import time as t
     global start
     start = t.time()
@@ -13,17 +13,13 @@ def worktimeend():
     total_m = int(total_ts // 60)
     if language == 'ru':
         firstwords = 'Программа выполнилась за'
-        if total_s % 10 == 1 and total_s < 5 or total_s > 19:
+        if total_s % 10 == 1 and total_s < 2 or total_s > 19:
             name_s = 'секунду'
-        if 1 < total_s % 10 < 5 and total_s < 5 or total_s > 19:
-            name_s = 'секунды'
-        if total_s % 10 > 5 or total_s % 10 == 0 and total_s > 5 or total_s < 19:
+        if total_s % 10 > 4 or total_s % 10 == 0 and total_s > 4 or total_s < 19:
             name_s = 'секунд'
-        if total_s % 10 == 1 and total_s < 5 or total_s > 19:
-            name_s = 'секунду'
         if 1 < total_s % 10 < 5 and total_s < 5 or total_s > 19:
             name_s = 'секунды'
-        if total_m % 10 > 5 or total_m % 10 == 0 and total_m > 5 or total_m < 19:
+        if total_m % 10 > 5 or total_m % 10 == 0 and total_m > 2 or total_m < 19:
             name_m = 'минут'
         if 1 < total_m % 10 < 5 and total_m < 5 or total_m > 19:
             name_m = 'минуты'
